21eee8c48f0365737a98929e3dc1624421690172
FacunMaldonado <facu.sl94@gmail.com>
2022-02-13 18:49:31 -0300

    Entrega

--- controllers/generosApiController.js
+++ controllers/generosApiController.js
@@ -3,26 +3,20 @@ const Op = DB.sequelize.Op;
 
 module.exports = {
     listar: async(req, res) => {
-        const getCancionesPorGenero = (id, canciones) =>canciones.filter(cancion => cancion.dataValues.genero_id === id); // Con esta funcion, filtramos las canciones por genero, devolvemos solo las canciones que sean del genero
-        
+        const getCancionesPorGenero = (id, canciones) =>canciones.filter(cancion => cancion.dataValues.genero_id === id);   
         try{
-        // Traemos los generos y canciones, usamos async/await para escribir menos codigo y que sea mas legible
         const generos = await DB.generos.findAll();
         const canciones = await DB.canciones.findAll();
 
         let cancionesPorGenero;
 
-        // Recorremos todos los generos, y por cada uno, filtramos las canciones que sean de ese genero
         generos.forEach(genero => {
-            // Y los guardamos en el objeto que sera la respuesta
             cancionesPorGenero = {
-                // Usamos el spread para no perder las propiedades que entran en el objeto con cada iteracion
                 ...cancionesPorGenero,
                 [genero.name]: getCancionesPorGenero(genero.id, canciones)
             }
         })
 
-        // Respondemos nuestro objeto poblado con las canciones por genero
         res.status(200).json(cancionesPorGenero);
         } catch(e){
             console.log(e);
